350826e2c5c857ba6c34a66773051cea885dfa4a
kanta_sasaki <k_padbjw@icloud.com>
2019-10-15 01:26:47 +0900

    エラー修正

--- app/src/main/java/com/example/locationalarmproject/MapsActivity.kt
+++ app/src/main/java/com/example/locationalarmproject/MapsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.locationalarmproject
 //
 //import android.graphics.Color
+//import android.location.LocationListener
 //import androidx.appcompat.app.AppCompatActivity
 //import android.os.Bundle
 //
@@ -13,15 +14,15 @@ package com.example.locationalarmproject
 //import com.google.android.gms.maps.model.LatLng
 //import com.google.android.gms.maps.model.MarkerOptions
 //import com.google.maps.android.SphericalUtil
-//
 
-//class MapsActivity : AppCompatActivity(), LocationListener {
+
+//class MapsActivity : AppCompatActivity(), LocationListener, {
 //    private lateinit var mMap: GoogleMap
 //
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_maps)
-//        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+////         Obtain the SupportMapFragment and get notified when whethe map is ready to be used.
 //        val mapFragment = supportFragmentManager
 //            .findFragmentById(R.id.map) as SupportMapFragment
 //        mapFragment.getMapAsync(this)
@@ -125,17 +126,34 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.GoogleMap
 
 
-class MapsActivity : AppCompatActivity(), LocationListener {
+import android.graphics.Color
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CircleOptions
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.SphericalUtil
+
+
+class MapsActivity : AppCompatActivity(), LocationListener,OnMapReadyCallback {
 
     // lateinit: late initialize to avoid checking null
     private lateinit var locationManager: LocationManager
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
 
+//        Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -154,6 +172,78 @@ class MapsActivity : AppCompatActivity(), LocationListener {
             }
 
         }
+    }
+
+    fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        // スワイプで地図を平行移動
+        googleMap.uiSettings.isScrollGesturesEnabled = true
+
+        // ピンチイン、ピンチアウトでズーム
+        googleMap.uiSettings.isZoomGesturesEnabled = true
+
+        // 回転
+        googleMap.uiSettings.isRotateGesturesEnabled = true
+
+        // ツールバー
+        googleMap.uiSettings.isZoomGesturesEnabled = true
+
+        // 2本指でスワイプで視点を傾けることができます。
+        googleMap.uiSettings.isMapToolbarEnabled = true
+
+        // コンパスの表示
+        googleMap.uiSettings.isTiltGesturesEnabled = true
+
+        // 現在地の表示
+        googleMap.uiSettings.isCompassEnabled = true
+
+        //現在位置の取得を許可
+        googleMap.setMyLocationEnabled(true);
+
+
+        // 自分の現在地に移動するアイコンの追加
+        googleMap.isMyLocationEnabled = true
+        val zoomValue = 14.0f // 1.0f 〜 21.0f を指定
+        var lastLatLng: LatLng? = null
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(lastLatLng, zoomValue))
+
+        // マーカーを表示させる.
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(sydney)             // 地図上のマーカーの位置
+                .title("Marker in Sydney")    // マーカーをタップ時に表示するテキスト文字列
+                .snippet("Australian cities") // タイトルの下に表示される追加のテキスト
+                .icon(BitmapDescriptorFactory.defaultMarker(
+                    BitmapDescriptorFactory.HUE_BLUE)) // アイコン
+        )
+
+        val latLng = LatLng(35.681236, 139.767125) // 東京駅
+        val radius = 1000 *1.0 // 1km
+
+        // 円を描画
+        googleMap.addCircle(
+            CircleOptions()
+                .center(latLng)          // 円の中心位置
+                .radius(radius)          // 半径 (メートル単位)
+                .strokeColor(Color.BLUE) // 線の色
+                .strokeWidth(2f)         // 線の太さ
+                .fillColor(0x400080ff)   // 円の塗りつぶし色
+        )
+
+        // 一旦例として東京駅と大阪駅の緯度と経度を示す
+        val latLngA = LatLng(35.681236, 139.767125)
+        val latLngB = LatLng(34.7331, 135.5002)
+
+        // 距離をメートル単位で返す
+        val distance = SphericalUtil.computeDistanceBetween(latLngA, latLngB)
+
+
     }
 
     private fun locationStart() {
